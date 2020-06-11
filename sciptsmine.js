d735f437cda8b8685a04636dcd8f6f8ce78a7d47
Noemike <66320530+Noemike@users.noreply.github.com>
2020-06-11 18:10:21 +0200

    done

--- sciptsmine.js
+++
@@ -1,7 +0,0 @@
-function changeBackground () {
-    let color=document.getElementById("input").value;
-    document.bgColor = color;
-    document.getElementById("button").style.color = color;
-}
-
-document.getElementById('button').addEventListener("click", changeBackground, false);
--- scripts.js
+++ scripts.js
@@ -1,29 +1,12 @@
-/*
-HTML
-
-let color= $('input').css("background-color", "blue")
-
-$('button').click (() => {
-    color
-});
-
-<input id='input' type="text">
-<button>Change color</button>
-    
-
-$('button').click (() => {
-    $('input').css("background-color","pink")
-});*/
-
-
-function changeBackground() {
-    var color = document.getElementById("color").value; // cached
-
-    // The working function for changing background color.
+/*function changeBackground () {
+    let color=document.getElementById("input").value;
     document.bgColor = color;
-
-    // The code I'd like to use for changing the text simultaneously - however it does not work.
-    document.getElementById("coltext").style.color = color;
+    document.getElementById("button").style.color = color;
 }
 
-document.getElementById("submitColor").addEventListener("click", changeBackground, false);
+document.getElementById('button').addEventListener("click", changeBackground, false);*/
+
+$('#btnChangeColor').click(() => {
+    let backGroundColor = $("#inpColor").val();
+    $("#btnChangeColor").css("background-color", backGroundColor)
+});
